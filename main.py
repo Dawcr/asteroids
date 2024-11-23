05e98225d0846736c7a7b63df8321acda971b3eb
dawcr <dawcraz@gmail.com>
2024-11-23 01:26:00 +0100

    Added circleshape.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         for event in pygame.event.get():
@@ -11,6 +13,8 @@ def main():
     
         screen.fill(000)
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
