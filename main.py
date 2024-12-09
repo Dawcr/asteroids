de3b21b66e3cd13008a93f1cfdb4d1b426908ada
dawcr <dawcraz@gmail.com>
2024-12-09 23:15:58 +0100

    grouped sprites

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ from constants import *
 from player import *
 
 def main():
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    
     
     while True:
         for event in pygame.event.get():
@@ -14,8 +22,10 @@ def main():
                 return
     
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        for obj in updatable:
+            obj.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
